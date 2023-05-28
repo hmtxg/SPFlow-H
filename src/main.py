ce58bca0c0f338738aab3644e5a86d9d137b23ac
Hannah <hmtx@uga.edu>
2023-05-28 17:52:24 -0400

    More clustering debugging

--- src/main.py
+++ src/main.py
@@ -4,9 +4,9 @@ import logging
 from spn.structure.StatisticalTypes import MetaType
 from spn.algorithms.RSPMNnewAlgo import RSPMNnewAlgo
 from spn.io.Graphics import plot_spn
-logging.basicConfig(filename="FL2.5KNA.log", level=logging.DEBUG)
+logging.basicConfig(filename="FLNA2.log", level=logging.DEBUG)
 
-csv_path = "/home/ht65490/Desktop/SPFlow-H/src/spn/RSPMN_MDP_Datasets/FrozenLake/FL2.5.csv"
+csv_path = "/home/ht65490/Desktop/SPFlow-H/src/spn/RSPMN_MDP_Datasets/FrozenLake/FrozenLake.csv"
 df = pd.read_csv(csv_path, sep=",", header=None)
 train_data = df.values
 
@@ -18,7 +18,7 @@ feature_names = [var for var_set in partial_order for var in var_set]
 meta_types = [MetaType.DISCRETE]*2+[MetaType.UTILITY]
 
 #Structure Learning
-rspmn = RSPMNnewAlgo(partial_order, decision_nodes, utility_nodes, feature_names, meta_types, cluster_by_curr_information_set=False, util_to_bin=False)
+rspmn = RSPMNnewAlgo(partial_order, decision_nodes, utility_nodes, feature_names, meta_types, cluster_by_curr_information_set=True, util_to_bin=False)
 wrapped_two_timestep_data = rspmn.InitialTemplate.wrap_sequence_into_two_time_steps(train_data)
 spmn_structure_two_time_steps, top_network, initial_template_network = rspmn.InitialTemplate.build_initial_template(wrapped_two_timestep_data)
 
@@ -29,9 +29,9 @@ template = rspmn.InitialTemplate.template_network
 template = rspmn.hard_em(train_data, template, False)
 
 #Plotting Learned Structure
-plot_spn(spmn_structure_two_time_steps, "/home/ht65490/Desktop/SPFlow/src/spn/RSPMN_Plots/FL2StepNA.pdf", feature_labels=["State0", "Action0", "Reward0", "State1", "Action1", "Reward1"])
-plot_spn(top_network, "/home/ht65490/Desktop/SPFlow/src/spn/RSPMN_Plots/FLTopNA.pdf", feature_labels=["State", "Action", "Reward"])
-plot_spn(initial_template_network, "/home/ht65490/Desktop/SPFlow/src/spn/RSPMN_Plots/FLTemplateNA.pdf", feature_labels=["State", "Action", "Reward"])
+plot_spn(spmn_structure_two_time_steps, "/home/ht65490/Desktop/SPFlow-H/src/spn/RSPMN_Plots/FL2StepNA2.pdf", feature_labels=["State0", "Action0", "Reward0", "State1", "Action1", "Reward1"])
+plot_spn(top_network, "/home/ht65490/Desktop/SPFlow-H/src/spn/RSPMN_Plots/FLTopNA2.pdf", feature_labels=["State", "Action", "Reward"])
+plot_spn(initial_template_network, "/home/ht65490/Desktop/SPFlow-H/src/spn/RSPMN_Plots/FLTemplateNA2.pdf", feature_labels=["State", "Action", "Reward"])
 
 #MEU
 print("MEU Calculations via value iteration")
